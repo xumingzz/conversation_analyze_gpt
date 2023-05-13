a47c4187b5c5a5f0d6e93dc5bce07d49f3253e6e
xuming.zz <3220676358@qq.com>
2023-05-14 01:22:07 +0800

    feat

--- demo.py
+++ demo.py
@@ -9,18 +9,17 @@ def Demo():
 
     analyzer = ConversationAnalyzer(chat=chat)
 
-    msg_analyzser_chain = ConvasationAnalyserChain(analyzer=analyzer)
+    analyzser_chain = ConvasationAnalyserChain(analyzer=analyzer)
 
-    msg_analyzser_chain_resp = {}
+    analyzser_chain_resp = {}
     with open("demo.txt", "r", encoding="utf-8") as file:
-        # 文件操作代码
         text = file.read()
         if len(text) > 0:
-            msg_analyzser_chain_resp = msg_analyzser_chain(inputs={"text": text[:5000]})
+            analyzser_chain_resp = analyzser_chain(inputs={"text": text[:5000]})
 
     analyze_list = []
-    if "text" in msg_analyzser_chain_resp:
-        analyze_list = msg_analyzser_chain_resp["text"]
+    if "text" in analyzser_chain_resp:
+        analyze_list = analyzser_chain_resp["text"]
 
     print(analyze_list)
 
